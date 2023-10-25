219753bf54b948d9602e153939ac5d0e27bdeff0
Lucia Aguzzoni <lucia.aguzzoni@gmail.com>
2023-10-25 20:23:26 +0200

    solutions

--- my_code.sql
+++ my_code.sql
@@ -37,17 +37,38 @@ SELECT category.name,
     ORDER BY average_duration DESC;
     
 -- 5
-SELECT title
+SELECT title,
+	   COUNT(inventory.film_id) as times_rented
 	FROM film
-		JOIN
-	ORDER BY COUNT() DESC;
+		JOIN inventory ON film.film_id = inventory.film_id
+	GROUP BY title
+	ORDER BY times_rented DESC;
+
     
--- 6
+-- 6 Answers: Sports, Sci-fi, Animation, Drama, Comedy
+SELECT category.name,
+	   SUM(amount) as gross_revenue
+	FROM category
+		JOIN film_category ON category.category_id = film_category.category_id
+        JOIN film ON film_category.film_id = film.film_id
+        JOIN inventory ON film.film_id = inventory.film_id
+        JOIN rental ON inventory.inventory_id = rental.inventory_id
+        JOIN payment ON rental.rental_id = payment.rental_id
+    GROUP BY category.name
+    ORDER BY gross_revenue DESC
+    LIMIT 5;
 
 
 -- 7
-
-	   
-
+SELECT  rental.inventory_id,
+        rental.rental_date,
+        return_date
+	FROM film
+		JOIN inventory ON film.film_id = inventory.film_id
+        JOIN rental ON inventory.inventory_ID = rental.inventory_id
+	WHERE film.title = 'Academy Dinosaur' AND inventory.store_id = 1
+    ORDER BY return_date ASC
+    LIMIT 1;
+-- Answers: return date = NULL -> not available
 
 
